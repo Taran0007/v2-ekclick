979891811e824243d422b400cf6324c294d1f1a1
Taran8080 <trnjeet8080@gmail.com>
2025-08-07 15:43:42 +0530

    Update config.php

--- config.php
+++ config.php
@@ -60,9 +60,14 @@ if ($environment === 'production') {
 // Session configuration
 session_start();
 
-// Error reporting (disable in production)
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// Error reporting (disable(idk)in production)
+if ($environment === 'production') {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+} else {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
 
 // Timezone
 date_default_timezone_set('UTC');
